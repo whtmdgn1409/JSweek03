6ed2452c4261f94a8948074ad21a775ae6a56783
Eunwoo <dmsdn960@gmail.com>
2019-11-17 20:01:41 +0900

    tokenizer lexer parser 순서대로 나눠서 구현

--- dom.js
+++ dom.js
@@ -53,10 +53,97 @@ function dom(html) {
   return JSON.stringify(result, null, 2);
 }
 
-// console.log(
-//   dom(
-//     "<html> <head> <title>자바스크립트 실행 컨텍스트와 스코프</title> </head> <body> <header> <h1>자바스크립트의 실행 컨텍스트와 스코프</h1> </header> <nav> <ul> <li>실행 컨텍스트</li> <li>스코프</li> </ul> </nav> <main> <ariticle> <h2>실행 컨텍스트</h2> <section> <p> 실행 컨텍스트란 자바스크립트 코드가 실행되고 연산되는 범위를 나타내는 추상적인 개념이다. </p> <p> 자바스크립트 엔진이 코드를 실행하기 위해서는 다음과 같은 정보를 알고 있어야한다. </p> <ul> <li>변수, 함수</li> <li>변수의 유효범위</li> <li>this</li> </ul> <p> 현재 코드가 실행되고 있는 범위에서 필요한 정보들을 관리해 주는 것이 실행 컨텍스트 이다. 실행컨텍스트의 종류에는 3가지가 있지만 현재는 두가지가 사용된다. </p> <ul> <li>Global Execution Context : script태그를 만날때 생성</li> <li>Functional Execution Context : 함수 호출 시 생성</li> <li><del>Eval Function Execution Context</del></li> </ul> </section> <section> <h4> 전역컨텍스트 </h4> <ul> <li>Variable Object : 변수 (전역변수, 전역함수)</li> <li>Scope Chain : [window]</li> <li>this : window</li> </ul> <h4> 함수컨텍스트 </h4> <ul> <li> Variable Object : 변수 (지역변수, 지역함수), 인자 (arguments) </li> <li>Scope Chain : [해당 함수의 활성화 객체, 전역 객체]</li> <li>this : window</li> </ul> <p>this는 new 키워드를 이용해 변경할 수 있다.</p> </section> <section> <h3>스코프</h3> <p> 현재 코드에서 접근 가능한 유효범위를 뜻한다. 현재 코드가 실행할 때의 컨텍스트가 유효한 범위를 뜻한다. 따라서 자바스크립트의 스코프는 전역 스코프와 함수 스코프로 나뉜다. </p> </section> </ariticle> </main> <footer> © 2019 닷제이에스 </footer> </body> </html>"
-//   )
-// );
+console.log(
+  regDom(
+    "<html> <head> <title>자바스크립트 실행 컨텍스트와 스코프</title> </head> <body> <header> <h1>자바스크립트의 실행 컨텍스트와 스코프</h1> </header> <nav> <ul> <li>실행 컨텍스트</li> <li>스코프</li> </ul> </nav> <main> <ariticle> <h2>실행 컨텍스트</h2> <section> <p> 실행 컨텍스트란 자바스크립트 코드가 실행되고 연산되는 범위를 나타내는 추상적인 개념이다. </p> <p> 자바스크립트 엔진이 코드를 실행하기 위해서는 다음과 같은 정보를 알고 있어야한다. </p> <ul> <li>변수, 함수</li> <li>변수의 유효범위</li> <li>this</li> </ul> <p> 현재 코드가 실행되고 있는 범위에서 필요한 정보들을 관리해 주는 것이 실행 컨텍스트 이다. 실행컨텍스트의 종류에는 3가지가 있지만 현재는 두가지가 사용된다. </p> <ul> <li>Global Execution Context : script태그를 만날때 생성</li> <li>Functional Execution Context : 함수 호출 시 생성</li> <li><del>Eval Function Execution Context</del></li> </ul> </section> <section> <h4> 전역컨텍스트 </h4> <ul> <li>Variable Object : 변수 (전역변수, 전역함수)</li> <li>Scope Chain : [window]</li> <li>this : window</li> </ul> <h4> 함수컨텍스트 </h4> <ul> <li> Variable Object : 변수 (지역변수, 지역함수), 인자 (arguments) </li> <li>Scope Chain : [해당 함수의 활성화 객체, 전역 객체]</li> <li>this : window</li> </ul> <p>this는 new 키워드를 이용해 변경할 수 있다.</p> </section> <section> <h3>스코프</h3> <p> 현재 코드에서 접근 가능한 유효범위를 뜻한다. 현재 코드가 실행할 때의 컨텍스트가 유효한 범위를 뜻한다. 따라서 자바스크립트의 스코프는 전역 스코프와 함수 스코프로 나뉜다. </p> </section> </ariticle> </main> <footer> © 2019 닷제이에스 </footer> </body> </html>"
+  )
+);
 
-console.log(dom("<html><head><link href=''/></head><body>aa</body></html>"));
+// console.log(regDom("<html><head><link href=''/></head><body>aa</body></html>"));
+
+function tokenizer(html) {
+  const reg = /(?:<\/?[\w]+\s?[^\/>]+?\/?>)|(?:[^<]+)/g;
+  const result = [];
+  let match;
+  while ((match = reg.exec(html))) {
+    result.push(match);
+  }
+  return result;
+}
+
+function lexer(tokens) {
+  return tokens.map(token => {
+    let [value] = token;
+    let type;
+    switch (true) {
+      case value.indexOf("</") >= 0:
+        value = value.substring(2, value.length - 1);
+        type = "closeTag";
+        break;
+      case value.indexOf("/>") >= 0:
+        value = value.substring(1, value.length - 2);
+        type = "completeTag";
+        break;
+      case value.indexOf("<") >= 0:
+        value = value.substring(1, value.length - 1);
+        type = "openTag";
+        break;
+      default:
+        type = "text";
+        break;
+    }
+    return {
+      value,
+      type
+    };
+  });
+}
+
+function parser(lexemes) {
+  const result = { type: "ROOT", children: [] };
+  let current = result.children;
+  let back;
+  lexemes.forEach(lexeme => {
+    let [tagName, ...attributes] = lexeme.value.split(" ");
+    attributes = attributes.reduce((res, cur) => {
+      const [key, value] = cur.split("=");
+      Object.assign(res, { [key]: value });
+      return res;
+    }, {});
+    switch (lexeme.type) {
+      case "openTag":
+        current.push({
+          tagName,
+          attributes,
+          type: "elementNode",
+          children: []
+        });
+        back = current;
+        current = current[current.length - 1].children;
+        break;
+      case "completeTag":
+        current.push({
+          tagName,
+          attributes,
+          type: "elementNode"
+        });
+        break;
+      case "closeTag":
+        current = back;
+        break;
+      case "text":
+        current.push({ value: lexeme.value, type: "textNode" });
+        break;
+      default:
+        break;
+    }
+  });
+  return result;
+}
+
+function regDom(html) {
+  const tokens = tokenizer(html);
+  const lexemes = lexer(tokens);
+  const tree = parser(lexemes);
+  return JSON.stringify(tree, null, 2);
+}
